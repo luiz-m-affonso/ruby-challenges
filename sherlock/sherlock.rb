1a9d14df887258f19ba92031afcdb03ec4a0cab1
Insight-Creator <luizbuchhalter@Outlook.com>
2020-09-21 11:08:55 -0300

    Updated Sherlock

--- sherlock/sherlock.rb
+++ sherlock/sherlock.rb
@@ -4,28 +4,24 @@ require 'json'
 require 'stringio'
 
 # Complete the isValid function below.
-def isValid(s)
-
+def valid?(s)
   counts = Hash.new(0)
   s.split('').each { |elem| counts[elem] += 1 }
 
-  sameRepetitionCount = Hash.new(0)
+  same_repetition_count = Hash.new(0)
   counts.each do |elem|
-    sameRepetitionCount[elem[1]] += 1
-    return 'NO' if sameRepetitionCount.size > 2
+    same_repetition_count[elem[1]] += 1
+    return 'NO' if same_repetition_count.size > 2
   end
 
-  if (sameRepetitionCount.size > 1 && sameRepetitionCount.key(1) != 1)
-    if sameRepetitionCount.key(1).nil?
+  if same_repetition_count.size > 1 && same_repetition_count.key(1) != 1
+    if same_repetition_count.key(1).nil?
       return 'NO'
     else
-      sameRepetitionCount[sameRepetitionCount.key(1) - 1] = sameRepetitionCount.delete sameRepetitionCount.key(1)
-      if sameRepetitionCount.size > 1
-        return 'NO'
-      end
+      same_repetition_count[same_repetition_count.key(1) - 1] = same_repetition_count.delete same_repetition_count.key(1)
+      return 'NO' if same_repetition_count.size > 1
     end
   end
-
   return 'YES'
 end
 
@@ -33,7 +29,7 @@ fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
 s = gets.to_s.rstrip
 
-result = isValid s
+result = valid? s
 
 fptr.write result
 fptr.write "\n"

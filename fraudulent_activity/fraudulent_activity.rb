1608d6eeb6591a0918c218cc7c4edfc3d2ac43c8
Insight-Creator <luizbuchhalter@Outlook.com>
2020-09-21 11:05:16 -0300

    Updated Challenges

--- fraudulent_activity/fraudulent_activity.rb
+++ fraudulent_activity/fraudulent_activity.rb
@@ -3,48 +3,45 @@
 require 'json'
 require 'stringio'
 
-# Complete the activityNotifications function below.
 def activity_notifications(expenditure, d, n)
   return 0 if d > n
-  # Assign Array in variable counter_activity_notifications
-  counter_activity_notifications = Array.new 201, 0
-  activity_notifications_count = 0
+  counter = Array.new 201, 0
+  count = 0
 
-  # Calling expenditure with method each_with_index it receive arguments activity_expense and index
-  expenditure.each_with_index do |activity_expense, index|
-    if index >= d
-      # Validate integers d and assign counter_activity_notifications with find_index method for array to variable median.
+  expenditure.each_with_index do |expense, expense_index|
+    if expense_index >= d
+      # Enough data to evalutate
       if d.odd?
-        median = counter_activity_notifications.find_index { |x| x >= (d / 2.0).ceil }
+        median = counter.find_index { |x| x >= (d / 2.0).ceil }
       else
-        previous = nil
-        counter_activity_notifications.each_with_index do |c, i|
+        prev = nil
+        counter.each_with_index do |c, i|
           if c > d / 2
-            median = previous.nil? ? i : (previous + i) / 2
+            median = prev.nil? ? i : (prev + i) / 2.0
+            break
           elsif c == d / 2
-            previous ||= i
+            prev ||= i
           end
         end
       end
 
-      activity_notifications_count += 1 if activity_expense >= 2 * median
+      count += 1 if expense >= 2 * median
 
-      activity_remove_expense = expenditure[index - d]
+      removed_expense = expenditure[expense_index - d]
 
-      if activity_expense > activity_remove_expense
-        activity_remove_expense.upto(activity_expense - 1) { |i| counter_activity_notifications[i] -= 1 }
-      elsif activity_expense < activity_remove_expense
-        activity_expense.upto(activity_remove_expense - 1) { |i| counter_activity_notifications[i] += 1 }
+      if expense > removed_expense
+        removed_expense.upto(expense - 1) { |i| counter[i] -= 1 }
+      elsif expense < removed_expense
+        expense.upto(removed_expense - 1) { |i| counter[i] += 1 }
       end
     else
-      # Collecting Spending Data with activity_expense with argument counter_activity_notifications
-      activity_expense.upto(200) { |i| counter_activity_notifications[i] += 1 }
+      # Still collecting spending data
+      expense.upto(200) { |i| counter[i] += 1 }
     end
   end
-  # Calling activity notifications count
-  activity_notifications_count
-end
 
+  count
+end
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
 nd = gets.rstrip.split
--- sherlock/sherlock.rb
+++ sherlock/sherlock.rb
@@ -3,31 +3,29 @@
 require 'json'
 require 'stringio'
 
-# Definition of Method isValid for a Valid string.
-def valid?(s)
-  # Assignment of hash in variable count with method string split for n elements in hash.
-  count = Hash.new(0)
-  s.split('').each do |n|
-    count[n] += 1
+# Complete the isValid function below.
+def isValid(s)
+
+  counts = Hash.new(0)
+  s.split('').each { |elem| counts[elem] += 1 }
+
+  sameRepetitionCount = Hash.new(0)
+  counts.each do |elem|
+    sameRepetitionCount[elem[1]] += 1
+    return 'NO' if sameRepetitionCount.size > 2
   end
 
-  # Assignment of hash in variable count_repetition
-  # with re-assign incrementation calling n with key in hash count_repetition
-  count_repetition = Hash.new(0)
-  count.each do |n|
-    count_repetition[n[1]] += 1
-    return 'NO' if count_repetition.size > 2
+  if (sameRepetitionCount.size > 1 && sameRepetitionCount.key(1) != 1)
+    if sameRepetitionCount.key(1).nil?
+      return 'NO'
+    else
+      sameRepetitionCount[sameRepetitionCount.key(1) - 1] = sameRepetitionCount.delete sameRepetitionCount.key(1)
+      if sameRepetitionCount.size > 1
+        return 'NO'
+      end
+    end
   end
 
-  # Conditional for size of count_repetition and for keys in count_repetition
-  if count_repetition.size > 1 && count_repetition.key(1) != 1
-    return 'NO' if count_repetition.key(1).nil?
-  else
-    # Calling count_repetition with key hash and decreasing
-    # Calling method delete for count_repetition and calling key in hash
-    count_repetition[count_repetition.key(1) - 1] = count_repetition.delete count_repetition.key(1)
-    return 'NO' if count_repetition.size > 1
-  end
   return 'YES'
 end
 
@@ -35,9 +33,9 @@ fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
 s = gets.to_s.rstrip
 
-result = valid? s
+result = isValid s
 
 fptr.write result
 fptr.write "\n"
 
-fptr.close
+fptr.close()
